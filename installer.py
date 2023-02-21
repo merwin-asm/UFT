7aab7251c3927b64c9c66282b7bd4d2200e728a7
Cactochan <85914469+mastercodermerwin@users.noreply.github.com>
2023-02-22 00:34:02 +0530

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,28 +1,94 @@
-import pyPath
 import os
-import time
+import stat
+import shutil
 
-my_path = pyPath.MYPATH()
+# MYPATH IS A MODULE MADE FOR LINUX. IT CAN BE USED TO
+# PERFORM PATH RELATED FUNCTIONS.
+# AUTHOR : MERWIN
 
-def reparsed_path(path):
-    path_final = ""
-    for each in str(path):
-        if each == " ":
-            path_final+="\ "
-        elif each == "(":
-            path_final+="\("
-        elif each == ")":
-            path_final += "\)"
+class MYPATH:
+    def __init__(self,):
+        pass
+    def make_redirecting_bashfile(self,filename,target_pyfile="",content_edited=None,allow_args=False,max_num_args=0):
+        if not allow_args:
+            default_command = f"""
+#!/bin/bash
+python3  {target_pyfile}
+        """
         else:
-            path_final+=each
-    return path_final
-try:
-    full_path_main = os.path.abspath("cli_uft.py")
-    full_path_main = reparsed_path(full_path_main)
-    my_path.make_redirecting_bashfile("uft",full_path_main,allow_args=True,max_num_args=5)
-    full_path = os.path.abspath("uft")
-    print(full_path)
-    my_path.addfile_to_path(full_path,"uft",exe=True)
-    print("INSTALLED...")
-except:
-    print("Failed or it has been already installed..")
+            sub = ""
+            for e in range(0,max_num_args):
+                sub += ' ${'
+                sub += f'args[{e}]'
+                sub += '} '
+            default_command = f"""
+#!/bin/bash
+args=("$@")
+python3  {target_pyfile} {sub}
+                    """
+        try:
+            file = open(filename, "x")
+            if content_edited != None:
+                file.write(content_edited)
+            else:
+                file.write(default_command)
+            file.close()
+        except:
+            file = open(filename, "w")
+            if content_edited != None:
+                file.write(content_edited)
+            else:
+                file.write(default_command)
+            file.close()
+    def addfile_to_path(self,path,file_name,exe=False,permissions=None):
+        target = rf'/usr/bin/{file_name}'
+        if os.path.isfile(path):
+            print(True)
+            shutil.copyfile(path,target)
+            os.system("cd /usr/bin")
+            if exe:
+                print(True)
+                os.chmod(target,0o755)
+            if permissions != None:
+                os.system(f"sudo chmod {permissions} {file_name}") #SET Permissions
+            return True
+        else:
+            return False
+    def seek_to_path(self):
+        return os.environ.get("PATH")
+    def path_var_exists(self,path):
+        path_vars = self.seek_to_path()
+        path_vars = path_vars.split(":")
+        for each in path_vars:
+           if each == path:
+               return True
+        return False
+    def add_dir_to_path_TEMP(self,path):
+        if os.path.isdir(path):
+            os.system(f'PATH="$PATH:{path}"')
+            return True
+        return False
+    def add_dir_to_path_PERMANENT(self,path,path_to_bashrc):
+        if os.path.isdir(path):
+            bashrc = open(path_to_bashrc,"a")
+            bashrc.write(f'PATH="$PATH:{path}"')
+            return True
+        return False
+
+    def reparsed_path(self,path):
+        path_final = ""
+        for each in str(path):
+            if each == " ":
+                path_final += "\ "
+            elif each == "(":
+                path_final += "\("
+            elif each == ")":
+                path_final += "\)"
+            else:
+                path_final += each
+        return path_final
+if __name__ == '__main__':
+    my_path = MYPATH()
+    # print(my_path.addfile_to_path("/home/merwin/programming/MYPATH/test_123_2.sh","test_123_2.sh",exe=True))
+    # print(my_path.seek_to_path())
+    # print(my_path.path_var_exists("/usr/bin"))
